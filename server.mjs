37fe0c5f987430b6b5d432d96eac7ea42a187b2d
shakeyiab <shakeyiabartell@gmail.com>
2024-11-11 20:03:13 -0500

    first commit

---
+++ server.mjs
@@ -0,0 +1,13 @@
+import express from 'express';
+
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Hello World</h1>`);
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on Port: ${PORT}`);
+  });
